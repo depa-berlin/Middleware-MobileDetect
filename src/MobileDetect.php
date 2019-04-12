73d580afdf00c29494290c9923d0b51b6976f04d
Mirko Fenrich <fenrich@designpark.de>
2019-04-13 00:22:49 +0200

    ...

--- src/MobileDetect.php
+++ src/MobileDetect.php
@@ -6,7 +6,7 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
-use Detection\MobileDetect;
+use Detection\MobileDetect; 
 
 class MobileDetect implements MiddlewareInterface
 {
